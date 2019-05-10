cca59bf79aa075eb4100b4a466a49c0f3b417ba1
JavierSLX <jserranolule@gmail.com>
2019-05-09 20:35:45 -0500

    Corrigiendo API

--- API-Rest/index.js
+++ API-Rest/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const productsApiRouter = require('./routes/api/products');
 const app = express();
-const bodyParse = require('body-parse');
+const bodyParser = require('body-parser');
 
 //Especifica las rutas de la API
 app.use('/api/products', productsApiRouter);
 
 //Permite procesas datos tipo JSON
-app.use(bodyParse.json());
+app.use(bodyParser.json());
 
 const server = app.listen(3000, () => {
     console.log(`Escuchando en el puerto ${server.address().port}...`);
